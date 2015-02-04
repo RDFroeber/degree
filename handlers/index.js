c0d249338e84ec0059c110560d6e806908f28087
Raleigh D. Froeber <Raleigh.Froeber@infor.com>
2015-02-04 12:15:29 -0500

    feat: Initial routing added and auth layer started

---
+++ handlers/index.js
@@ -0,0 +1,44 @@
+// Route Handlers and Configs
+var Joi = require('joi');
+
+module.exports = {
+  helloWorld: function(request, reply) {
+    reply('Hello, world! ' + request.auth.credentials);
+  },
+
+  helloConfig: {
+    handler: function(request, reply) {
+      var names = request.params.name.split("/");
+      reply({
+        first: names[0],
+        last: names[1],
+        mood: request.query.mood,
+        age: request.query.age
+      });
+    },
+    validate: {
+      params: {
+        name: Joi.string().min(8).max(100)
+      },
+      query: {
+        mood: Joi.string().valid(['neutral','happy','sad']).default('neutral'),
+        age: Joi.number().integer().min(13).max(100)
+      }
+    }
+  },
+
+  google: {
+    auth: {
+      strategy: 'google',
+      mode: 'try' // What is this?
+    },
+    handler: function (request, reply) {
+      if (!request.auth.isAuthenticated) {
+        return reply('Authentication failed due to: ' + request.auth.error.message);
+      }
+      console.log('request', request)
+      // request.auth.google.set(request.auth.credentials.token);
+      return reply.redirect('/');
+    }
+  }
+}
--- server.js
+++ server.js
@@ -1,14 +1,50 @@
-var Composer = require('./index');
+var Hapi = require('hapi'),
+    Good = require('good'),
+    Bell = require('bell'),
+    AuthCookie = require('hapi-auth-cookie');
 
+var auth = require('./auth');
+var server = new Hapi.Server();
 
-Composer(function (err, server) {
+server.connection({ port: 8080 });
 
-  if (err) {
+server.register([
+  {register: require('lout')},
+  {register: Bell},
+  {register: AuthCookie},
+  {register: Good,
+    options: {
+      reporters: [{
+        reporter: require('good-console'),
+        args:[{ log: '*', response: '*' }]
+      }]
+    }
+  }
+], function (err) {
+  if(err) {
     throw err;
   }
 
-  server.start(function () {
+  server.auth.strategy('google', 'bell', {
+    provider: 'google',
+    cookie: 'sid-ex-degree',
+    password: 'gettingKnowledge',
+    isSecure: false,
+    clientId: auth.Google.clientId,
+    clientSecret: auth.Google.clientSecret,
+    providerParams: {
+      redirect_uri: server.info.uri + '/bell/door'
+    }
+  });
 
-    console.log('Started the plot device.');
+  server.start(function () {
+    server.log('info', 'Server running at: ' + server.info.uri);
   });
 });
+
+// Routes
+var handler = require('./handlers');
+
+server.route({path: '/', method:'GET', handler: handler.helloWorld});
+server.route({path: '/hello/{name*2}', method: 'GET', config: handler.helloConfig});
+server.route({method: ['GET', 'POST'], path: '/bell/door', config: handler.google});
