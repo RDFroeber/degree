0ae4bf1b4d119625cf75509b4095ddb0531697ca
Raleigh D. Froeber <Raleigh.Froeber@infor.com>
2015-02-06 01:18:45 -0500

    feat: Degree and course controllers added

---
+++ controllers/courseCtrl.js
@@ -0,0 +1,95 @@
+'use strict';
+
+/**
+ * Course Controller
+ **/
+
+var Course = require('../models/Course');
+
+module.exports = {
+
+  create: function(request, reply){
+    var Course = request.payload;
+
+    Course.create(course, function(err, newCourse){
+      if(err){
+        console.log(err);
+      } else {
+        return reply(newCourse).code(201);
+      }
+    });
+  },
+
+  findById: function(request, reply){
+    var id = request.params.id;
+
+    Course.findById(id).exec(function(err, course){
+      if(err){
+        return reply(err).code(400);
+      } else if(!course){
+        return reply('Course Not Found').code(404);
+      } else {
+        return reply(course).code(200);
+      }
+    });
+  },
+
+  findByNumber: function(request, reply){
+    var number = request.params.number;
+
+    Course.findOne({'number': number}).exec(function(err, course){
+      if(err){
+        return reply(err).code(400);
+      } else if(!course){
+        return reply('Course Not Found').code(404);
+      } else {
+        return reply(course).code(200);
+      }
+    });
+  },
+
+  findByName: function(request, reply){
+    var name = request.params.name;
+
+    Course.findOne({'name': name}).exec(function(err, course){
+      if(err){
+        return reply(err).code(400);
+      } else if(!course){
+        return reply('Course Not Found').code(404);
+      } else {
+        return reply(course).code(200);
+      }
+    });
+  },
+
+  updateById: function(request, reply){
+    var id = request.params.id,
+        course = request.payload;
+  
+    Course.findByIdAndUpdate(id, course).exec(function(err, updatedCourse){
+      if(err){
+        return reply(err).code(400);
+      } else if(!updatedCourse){
+        return reply('Course Not Found').code(404);
+      } else {
+        return reply(updatedCourse).code(200);
+      }
+    });
+  },
+
+  hardDelete: function(request, reply){
+    var id = request.params.id;
+  
+    Course.findByIdAndRemove(id).exec(function(err, removed){
+      if(err){
+        return reply(err).code(400);
+      } else if(!removed){
+        return reply('Course Not Found').code(404);
+      } else {
+        return reply().code(204);
+      }
+    });
+  },
+
+
+ }
---
+++ controllers/degreeCtrl.js
@@ -0,0 +1,81 @@
+'use strict';
+
+/**
+ * Degree Controller
+ **/
+
+var Degree = require('../models/Degree');
+
+module.exports = {
+
+  create: function(request, reply){
+    var Degree = request.payload;
+
+    Degree.create(degree, function(err, newDegree){
+      if(err){
+        console.log(err);
+      } else {
+        return reply(newDegree).code(201);
+      }
+    });
+  },
+
+  findById: function(request, reply){
+    var id = request.params.id;
+
+    Degree.findById(id).exec(function(err, degree){
+      if(err){
+        return reply(err).code(400);
+      } else if(!degree){
+        return reply('Degree Not Found').code(404);
+      } else {
+        return reply(degree).code(200);
+      }
+    });
+  },
+
+  findByTrack: function(request, reply){
+    var track = request.params.track;
+
+    Degree.findOne({'tracks.name': track}).exec(function(err, degree){
+      if(err){
+        return reply(err).code(400);
+      } else if(!degree){
+        return reply('Degree Not Found').code(404);
+      } else {
+        return reply(degree).code(200);
+      }
+    });
+  },
+
+  updateById: function(request, reply){
+    var id = request.params.id,
+        degree = request.payload;
+  
+    Degree.findByIdAndUpdate(id, degree).exec(function(err, updatedDegree){
+      if(err){
+        return reply(err).code(400);
+      } else if(!updatedDegree){
+        return reply('Degree Not Found').code(404);
+      } else {
+        return reply(updatedDegree).code(200);
+      }
+    });
+  },
+
+  hardDelete: function(request, reply){
+    var id = request.params.id;
+  
+    Degree.findByIdAndRemove(id).exec(function(err, removed){
+      if(err){
+        return reply(err).code(400);
+      } else if(!removed){
+        return reply('Degree Not Found').code(404);
+      } else {
+        return reply().code(204);
+      }
+    });
+  },
+
+
+ }
